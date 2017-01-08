59ec7e652f703c118feaa696998d43db8c9dd20e
djelusic <djelusic133@gmail.com>
2017-01-08 21:30:20 +0100

    Fix array initialization to be compiler-independent.

--- GBEmu/Memory.cpp
+++ GBEmu/Memory.cpp
@@ -62,12 +62,12 @@ void Memory::ChangeBanks(const word & address, const byte & val) {
 Memory::Memory(GB *gb, const char* rom_fname, Controller &controller) : 
 	gb(gb), 
 	controller(controller) {
-	m_MMU = new byte[0x10000]{};
-	m_Cartridge = new byte[0x800000]{};
-	m_CartridgeRAM = new byte[0x20000]{};
-	m_VRAM = new byte[0x4000]{};
-	m_WRAM = new byte[0x8000]{};
-	m_PaletteData = new byte[0x80]{};
+	m_MMU = new byte[0x10000]();
+	m_Cartridge = new byte[0x800000]();
+	m_CartridgeRAM = new byte[0x20000]();
+	m_VRAM = new byte[0x4000]();
+	m_WRAM = new byte[0x8000]();
+	m_PaletteData = new byte[0x80]();
 	
 	for (int i = 0; i < 0x40; i++) {
 		m_PaletteData[i] = 0xFF;
